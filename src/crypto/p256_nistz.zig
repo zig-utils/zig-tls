d82fec91b37c0310a1a370f2d844459a5e10c200
Chris <chrisbreuer93@gmail.com>
2026-06-08 11:41:30 -0700

    fix(crypto): skip zero Booth digits in nistz mulBase
    Gueron–Krasnov comb windows with mag==0 must not double the accumulator;
    that broke scalars like 128×G and inflated cert-handshake cost.

--- src/crypto/p256_nistz.zig
+++ src/crypto/p256_nistz.zig
@@ -6,7 +6,6 @@ const crypto = std.crypto;
 const p256 = @import("p256.zig");
 const P256 = p256.P256;
 const Fe = P256.Fe;
-const AffineCoordinates = p256.AffineCoordinates;
 const table = @import("p256/nistz_table.zig");
 
 pub const enabled = builtin.cpu.arch == .aarch64 or builtin.cpu.arch == .x86_64;
@@ -52,15 +51,7 @@ pub fn mulBase(s: [32]u8) IdentityElementError!P256 {
         const wvalue = boothRecodeW7(loadWindow(s, row_i));
         const mag: usize = @intCast(wvalue >> 1);
 
-        if (mag == 0) {
-            if (!ret_is_zero) {
-                ret = ret.dbl();
-            } else {
-                ret = P256.identityElement;
-                ret_is_zero = false;
-            }
-            continue;
-        }
+        if (mag == 0) continue;
 
         const pt = selectAffine(table.ecp_nistz256_precomputed[row_i], mag - 1, row_i);
         var y = feFromMontgomeryLimbs(pt.y);
@@ -77,6 +68,19 @@ pub fn mulBase(s: [32]u8) IdentityElementError!P256 {
 
     try ret.rejectIdentity();
     return ret;
+}
+
+test "nistz mulBase matches mulPublic" {
+    var s_be: [32]u8 = @splat(0);
+    var n: u32 = 1;
+    while (n < 500) : (n += 1) {
+        @memset(&s_be, 0);
+        std.mem.writeInt(u32, s_be[28..32], n, .big);
+        const s = Fe.orderSwap(s_be);
+        const a = try mulBase(s);
+        const b = try P256.basePoint.mulPublic(s_be, .big);
+        try std.testing.expect(a.equivalent(b));
+    }
 }
 
 test "nistz base multiply matches scalar 1" {
